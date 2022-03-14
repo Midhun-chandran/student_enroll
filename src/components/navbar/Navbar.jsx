7bbf8a506d9dab8ec004045ede9e6aebef646ebb
Midhun-chandran <midhun.gitlab@gmail.com>
2022-03-14 14:47:42 +0530

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,10 +19,12 @@ export default function Navbar() {
     <div className={user && user.isAdmin ? "navbar1 admin " : "navbar1"}>
       <div className="container-fluid  ">
         <div className="left">
+          <Link to='/'>
           <img
             src="https://ictkerala.org/wp-content/uploads/2019/01/cropped-ict-ico.png"
             alt=""
           />
+          </Link>
           <span>
             <Link className="link" to="/">
               HOME
