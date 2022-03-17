5edd0cb585f4c1df968d6c1b37f8c4aae818a0b9
Midhun-chandran <midhun.gitlab@gmail.com>
2022-03-17 12:36:26 +0530

    commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Courses from "../../components/courses/Courses";
+
 import FrontSection from "../../components/frontSection/FrontSection";
 import MiddleSection from "../../components/middleSection/MiddleSection";
 import LastSection from "../../components/lastSection/LastSection.jsx";
 
 import "./home.scss";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
+
 
 const Home = () => {
 
