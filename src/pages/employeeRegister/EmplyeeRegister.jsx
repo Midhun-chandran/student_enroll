f96e99533cb6ee0d108f5e7defc25921b724ee23
Midhun-chandran <midhun.gitlab@gmail.com>
2022-03-18 11:18:31 +0530

    Update EmplyeeRegister.jsx

--- src/pages/employeeRegister/EmplyeeRegister.jsx
+++ src/pages/employeeRegister/EmplyeeRegister.jsx
@@ -1,5 +1,4 @@
 import "./employeeRegister.css";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
 import { pattern } from "../../validation/validation";
