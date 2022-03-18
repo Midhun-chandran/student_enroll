0c7cea9df598ee493fd612c7e53cf42c86a0234c
Midhun-chandran <midhun.gitlab@gmail.com>
2022-03-18 10:52:32 +0530

    navbar changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,16 +2,13 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import "./navbar.css";
-
 export default function Navbar() {
   const { user, dispatch } = useContext(Context);
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
   };
-
-  return (
-
-  <nav className="navbar fixed-top navbar-expand-lg  navbar-dark bg-dark">
+   return (
+     <nav className="navbar fixed-top navbar-expand-lg  navbar-dark bg-dark">
   <div className="container-fluid">
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -21,109 +18,102 @@ export default function Navbar() {
                 alt=""
               style={{width:"35px"}} /></Link>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
  
-        <li className="nav-item me-4 ">
+        <li className="nav-item me-3">
            <Link to='/' className="nav-link active" aria-current="page" >HOME</Link>
         </li>
-        
-       
+         <li className="nav-item me-3">
+           <Link to='/courses' className="nav-link active" aria-current="page" >COURSES</Link>
+        </li>
+
+
         {!user && (
-           <li class="nav-item dropdown  me-4">
-             <Link  to='/' class="nav-link  active" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           <li className="nav-item me-3 dropdown">
+             <a className="nav-link  active"  href='/' id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             LOGIN
-          </Link>
-            <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
-               <li><Link  class="dropdown-item" to="/employee-login">EMPLOYEE</Link></li>
-               <li><Link  class="dropdown-item" to="/admin-login"> ADMIN</Link></li>
+          </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+               <li><a className="dropdown-item" href="/employee-login">EMPLOYEE</a></li>
+               <li><a className="dropdown-item" href="/admin-login"> ADMIN</a></li>
           
-              <li><Link  class="dropdown-item" to="/student-login"> STUDENT</Link></li>
+              <li><a className="dropdown-item" href="/student-login"> STUDENT</a></li>
             </ul>
           </li>
          )}
 
          {!user && (
 
-         <li class="nav-item dropdown me-4 ">
-            <Link to='/' class="nav-link  active "  id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+         <li className="nav-item me-3 dropdown">
+            <a className="nav-link  active " href='/' id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             REGISTER
-          </Link>
-          <ul class="dropdown-menu " aria-labelledby="navbarScrollingDropdown">
-            <li><Link class="dropdown-item " to="/student-register">STUDENT</Link></li>
-            <li><Link class="dropdown-item" to="/employee-register">EMPLOYEE</Link></li>
+          </a>
+          <ul className="dropdown-menu " aria-labelledby="navbarScrollingDropdown">
+            <li><a className="dropdown-item " href="/student-register">STUDENT</a></li>
+            <li><a className="dropdown-item" href="/employee-register">EMPLOYEE</a></li>
           
        
           </ul>
-        </li>
+           </li>
           )}
-
-          
-
-        
-           {user && user.isAdmin && (
-          <li className="nav-item me-4">
+                {user && user.isAdmin && (
+          <li className="nav-item me-3">
            <Link to='/course' className="nav-link active" aria-current="page" >ADD COURSES</Link>
-        </li>
+               </li>
           )}
-         
-        
-         <li className="nav-item me-4">
-           <Link to='/courses' className="nav-link active" aria-current="page" >COURSES</Link>
-        </li> 
-
-        
            {user && user.isAdmin && (
-          <li className="nav-item me-4">
+          <li className="nav-item me-3">
            <Link to='/courses' className="nav-link active" aria-current="page" > EMPLOYEES</Link>
-        </li>
+              </li>
           )}
-
 
             {((user && user.isAdmin) || (user && user.isEmployee)) && (
-          <li className="nav-item me-4">
+          <li className="nav-item me-3">
            <Link to='/search' className="nav-link active" aria-current="page" >  SEARCH STUDENTS</Link>
-        </li>
+            </li>
           )}
-
            {user && user.isAdmin && (
-          <li className="nav-item me-4">
+          <li className="nav-item me-3">
            <Link to='/approve' className="nav-link active" aria-current="page" > APPROVE STUDENTS</Link>
-        </li>
+              </li>
           )}
 
-          {user && user.isAdmin && (
-          <li className="nav-item ">
+               {user && user.isAdmin && (
+              <li className="nav-item me-3">
            <Link to='/employee-approve' className="nav-link active" aria-current="page" > APPROVE EMPLOYEES</Link>
-        </li>
-          )}
-           </ul>
-           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+             </li>
+             )}
+          
+          </ul>
+           <ul className="navbar-nav  mb-2 mb-lg-0">
 
           {user && (
           <>
                {user && user.isStudent && (
-                <li className="nav-item me-4">
+                <li className="nav-item me-3">
                   <Link to={`/student/${user._id || ""}`} className="nav-link active" aria-current="page" > MY PROFILE</Link>
                </li>
-          )}
-
-           {user && user.isEmployee && (
-              <li className="nav-item right me-4">
+                )}
+           
+               {user && user.isEmployee && (
+              <li className="nav-item me-3 right ">
                <Link to={`/employee/${user._id || ""}`} className="nav-link active" aria-current="page" > MY PROFILE</Link>
              </li>
-            )}
-         <li class="nav-item right me-4 ">
-             <Link class="nav-link disabled">{user.username}</Link>
-        </li>
+              )}
+
+             <li className="nav-item me-3 right ">
+             <a className="nav-link disabled" href='/'>{user.username}</a>
+          </li>
      
-       <li className="nav-item me-4" onClick={handleLogout}>
+                   <li className="nav-item me-3" onClick={handleLogout}>
            <Link to='/' className="nav-link active " aria-current="page" > LOGOUT</Link>
-        </li>
-       </>
-        )}
-       </ul>  
-      
+                    </li>
+             </>
+             )}
+         </ul>  
+     
     </div>
   </div>
 </nav>  );
 }
+          
